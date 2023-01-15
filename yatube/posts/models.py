c5a6adc86525d3e55c57a62cb7b40e040d16e382
Azhur <showipbgpall2@gmail.com>
2023-01-15 17:54:38 +0500

    isort *

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 User = get_user_model()
+NUMB_SIBM = 30
 
 
 class Group(models.Model):
@@ -38,4 +39,4 @@ class Post(models.Model):
         default_related_name = 'posts_rname'
 
     def __str__(self):
-        return self.text
+        return self.text[:NUMB_SIBM]
